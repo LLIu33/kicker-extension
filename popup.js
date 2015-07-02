bc31495c3a3b62318abcd42b9811f41ac39303f8
tuocs <artem@intspirit.com>
2015-07-02 20:09:33 +0300

    Added parsing json with data

--- popup.js
+++ popup.js
@@ -1,13 +1,67 @@
-$(document).ready(function() { 
+'use strict';
+/*global $ */
+$(document).ready(function() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://kicker-parser.herokuapp.com/", true);
+    xhr.open('GET', 'https://kicker-parser.herokuapp.com/', true);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        // WARNING! Might be evaluating an evil script!
-        //var resp = eval("(" + xhr.responseText + ")");
-        $("status").text(xhr.responseText);
-        console.log(xhr.responseText);
-      }
-    }
+        if (xhr.readyState === 4) {
+            // WARNING! Might be evaluating an evil script!
+            //var resp = eval("(" + xhr.responseText + ")");
+            // $('status').text(xhr.responseText);
+            var target = $('#status pre');
+            var result = JSON.parse(xhr.responseText);
+            console.log(result);
+            $.each(result.taverns, function(i, item) {
+                if (item.usersInside.length > 0 || item.usersPlanToPlay.length > 0) {
+                    var tavernEl = $('<div></div>');
+                    tavernEl.appendTo(target);
+                    var tavernInfo = $('' + 
+                        '<div style="padding: 0 10px;">' +
+                            '<img src="http://mytogo.ru' + item.logo + '" width=40 height=40 /> ' + 
+                            '<span>' + item.name + '</span>' + 
+                        '</div>'
+                        );
+                    tavernEl.append(tavernInfo);
+
+                    if (item.usersInside.length > 0) {
+                        var userBlockEl = $('div');
+                        userBlockEl.append('<h3>').text('Сейчас в заведении:');
+                        var userStr = $('<div>');
+                        $.each(item.usersInside, function() {
+                            var el = this;
+                            userStr.append(''+
+                                '<img src="http://mytogo.ru' + el.avatar + '" width=40 height=40 /> ' + 
+                                '<div style="float:right;">' +
+                                    '<span>' + el.name + '</span>'+
+                                '</div>'
+                            );
+                        });
+                        userBlockEl.append(userStr);
+                        tavernEl.append(userBlockEl);
+                    }
+                    
+                    if (item.usersPlanToPlay.length > 0) {
+                        var userPlanBlockEl = $('div');
+                        userPlanBlockEl.append('<h3>').text('Планируют быть:');
+                        var userPlanStr = $('<div>');
+                        $.each(item.usersPlanToPlay, function() {
+                            var el = this;
+                            userPlanStr.append(''+
+                                '<img src="http://mytogo.ru/' + el.avatar + '" width=40 height=40 /> ' + 
+                                '<div style="float:right;">' +
+                                    '<span><b>' + el.dateOfPlan + '</b></span><br />' +
+                                    '<span>' + el.name + '</span>' +
+                                '</div>'
+                            );
+                        });
+                        userPlanBlockEl.append(userPlanStr);
+                        tavernEl.append(userPlanBlockEl);
+                    }
+                }
+            });
+        console.log(target.html());
+        console.log($('#status'));
+        }
+    };
     xhr.send();
 });
