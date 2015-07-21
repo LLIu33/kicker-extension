10506bb3f375283b3c1af33092b0ac4243d0d0fa
tuocs <artem@intspirit.com>
2015-07-21 18:58:01 +0300

    Cleanup repo. Changed styling. Changed rendering. Added icon

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,70 +1,66 @@
 'use strict';
 /*global $, tmpl */
 
-var getNowStr = function() {
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+var renderData = function(contentWrapper, contentBlock, spinner, result) {
+    contentWrapper.find('.refresh-time').text(result.time);
+    spinner.hide();
+    contentBlock.empty().show();
+    $.each(result.taverns, function(i, item) {
+        if (item.usersInside.length > 0 || item.usersPlanToPlay.length > 0) {
+            var tavernBlockHtml = '';
+            var tavernTemplate = tmpl('tavern_tmpl');
+            tavernBlockHtml += tavernTemplate(item);
 
-    var yyyy = today.getFullYear();
-    if(dd<10){
-        dd='0'+dd;
-    } 
-    if(mm<10){
-        mm='0'+mm;
-    } 
-    return dd+'/'+mm+'/'+yyyy;
-};
-
-var getData = function(city) {
-    $.get('https://kicker-parser.herokuapp.com#' + city, function(data) {
-        console.log(data);
-        console.log(getNowStr());
-})
-    .done(function(result) {
-        // Hide "Please wait" string
-        var infoBlock = $('.info').hide();
-        var contentWrapper = $('.tab-pane.active').empty();
-        contentWrapper.html(tmpl('panel_tmpl', {}));
-        var contentBlock = contentWrapper.find('.panel-body .row');
-        $.each(result.taverns, function(i, item) {
-            if (item.usersInside.length > 0 || item.usersPlanToPlay.length > 0) {
-                var tavernBlockHtml = '';
-                var tavernTemplate = tmpl('tavern_tmpl');
-                tavernBlockHtml += tavernTemplate(item);
-
-                if (item.usersInside.length > 0) {
-                    $.each(item.usersInside, function(i, user) {
-                        var userTemplate = tmpl('user_tmpl');
-                        tavernBlockHtml += userTemplate(user);
-                    });
-                }
-
-                if (item.usersPlanToPlay.length > 0) {
-                    $.each(item.usersPlanToPlay, function(i, user) {
-                        var userTemplate = tmpl('user_tmpl');
-                        tavernBlockHtml += userTemplate(user);
-                    });
-                }
-                contentBlock.html(tavernBlockHtml);
+            if (item.usersInside.length > 0) {
+                $.each(item.usersInside, function(i, user) {
+                    var userTemplate = tmpl('user_tmpl');
+                    tavernBlockHtml += userTemplate(user);
+                });
             }
-        });
+
+            if (item.usersPlanToPlay.length > 0) {
+                $.each(item.usersPlanToPlay, function(i, user) {
+                    var userTemplate = tmpl('user_tmpl');
+                    tavernBlockHtml += userTemplate(user);
+                });
+            }
+            contentBlock.append(tavernBlockHtml);
+        }
     });
 };
+
+var getData = function() {
+    var contentWrapper = $('.tab-pane.active');
+    var city = contentWrapper.attr('id');
+    var contentBlock = contentWrapper.find('.panel-body .row').hide();
+    var spinner = contentWrapper.find('.refresh-container').show();
+    $.ajax({
+        method: 'POST',
+        // url: 'https://kicker-parser.herokuapp.com',
+        url: 'http://localhost:5000',
+        data: { city: city },
+        dataType: 'json'
+    })
+    .done(function(result) {
+        console.log(result);
+        renderData(contentWrapper, contentBlock, spinner, result);
+    });
+};
+
+
 
 $(document).ready(function() {
-    $('.refresh-button').refreshMe({
-        started: function(ele) { ele.html('Please wait...'); },
-        completed: function(ele) { ele.html('Data is was updated : ' + getNowStr()); },
-    });
-
-    $('.nav.nav-tabs li a').click(function (e) {
-        var el = $(this);
-        var city = el.data('city');
-        e.preventDefault();
-        el.tab('show');
-        getData(city);
-    });
-
+    //initialize
     getData();
+
+    $('.nav.nav-tabs li a').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+        getData();
+    });
+
+    $('.refresh-button').click(function() {
+        getData();
+    });
+
 });
